99cd1992a73d9f0323eee42bd5383f42d1d58c90
RNK302 <110764622+RNK302@users.noreply.github.com>
2024-05-03 23:48:28 +0530

    Create new.java
    This an input get from an user in the int or string we can change by modify an keyword

---
+++ new.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class new 
+{
+public static void main (String args[])
+{
+Scanner sc= new scanner(System.in);
+System.out.println("Enter your value");
+int value = sc.nextInt();
+if(values == 3)
+{
+System.out.println("Your values" + value);
+}
+else
+{
+System.out.println("you values is incorrect");
+}
+}
+}
