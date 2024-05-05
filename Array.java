b6b8550aeeba08e7d4c6d97194bd0aa491279937
RNK302 <110764622+RNK302@users.noreply.github.com>
2024-05-05 11:25:13 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,4 +1,26 @@
+import java.util.*;
 class Array
   {
-    p
-  }
+    public static void main(String args[])
+{
+    Scanner sc =new Scanner(System.in);
+    System.out.println("enter row");
+    int row=sc.nextInt();
+    System.out.println("enter col");
+    int col=sc.nextInt();
+    int [][] a =new int[row][col]
+   for(int i=0;i<row;i++){
+   for(int j=0;j=col;j++){
+   a[i][j]=(i+1)*(j+1);
+}
+}
+   for(int i=0;i<row;i++){
+   for(int j=0;j=col;j++){
+   System.out.println(a[i][j]+" ");
+}
+}
+
+
+    
+}
+}
