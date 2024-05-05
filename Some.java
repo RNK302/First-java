88218694f459fff4a1fca498cc864bd6ab7f5d5f
RNK302 <110764622+RNK302@users.noreply.github.com>
2024-05-05 22:06:27 +0530

    Update Some.java

--- Some.java
+++ Some.java
@@ -1,3 +1,12 @@
+import java.util.*
 class some
   {
+public static void main(String args)
+{
+Scanner sc = new scanner (System in);
+System.out.println("Enter value");
+int value=sc.nextInt();
+if(value/2)
+
+
     
