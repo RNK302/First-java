7d902a9beb38bf84211ba9f8bc70b67f45c9d8c0
RNK302 <110764622+RNK302@users.noreply.github.com>
2024-05-06 22:07:28 +0530

    Update Some.java

--- Some.java
+++ Some.java
@@ -6,7 +6,15 @@ public static void main(String args)
 Scanner sc = new scanner (System in);
 System.out.println("Enter value");
 int value=sc.nextInt();
-if(value/2)
-
-
+if(value/2==0)
+{
+ System.out.println("Value is prime no:"+value);
+}
+else
+{
+System.out.println("value is not prime no");
+}
+sc.close();
+}
+}
     
